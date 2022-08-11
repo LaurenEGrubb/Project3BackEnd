edddf3e127319f528362f84234ff63c51d4efb3d
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-08-11 13:43:06 -0400

    created AuthController

--- middleware/index.js
+++ middleware/index.js
@@ -16,3 +16,36 @@ const hashPassword = async (password) => {
     let token = jwt.sign(payload, `${APP_SECRET}`)
     return token
   }
+  const verifyToken = (req, res, next) => {
+    const { token } = res.locals
+    let payload = jwt.verify(token, `${APP_SECRET}`)
+    if (payload) {
+      res.locals.payload = payload 
+  
+    
+      return next()
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+  
+  const stripToken = (req, res, next) => {
+    try {
+      const token = req.headers['authorization'].split(' ')[1]
+      if (token) {
+        res.locals.token = token
+       
+        return next()
+      }
+    } catch (error) {
+      res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+    }
+  }
+  
+  module.exports = {
+    stripToken,
+    verifyToken,
+    createToken,
+    comparePassword,
+    hashPassword
+  }
+  
