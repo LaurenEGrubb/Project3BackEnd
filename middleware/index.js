159c6bd5a31cf3f620e3e2b1b12e0493eedbcaa0
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-08-11 13:38:23 -0400

    middleware for password controls

---
+++ middleware/index.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require("dotenv").config()
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
+const APP_SECRET = process.env.APP_SECRET
+
+const hashPassword = async (password) => {
+    let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+    return hashedPassword
+  }
+  const comparePassword = async (storedPassword, password) => {
+    let passwordMatch = await bcrypt.compare(password, storedPassword)
+    return passwordMatch
+  }
+  const createToken = (payload) => {
+    let token = jwt.sign(payload, `${APP_SECRET}`)
+    return token
+  }
