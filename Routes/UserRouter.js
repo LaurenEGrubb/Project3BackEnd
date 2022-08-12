238d847e4f6b518fa788d40c25aec0ffe9b36e31
laurengrubb <lauren.elyse.grubb@gmail.com>
2022-08-11 22:00:57 -0400

    merging conflict resolve

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,27 +1,19 @@
 const Router = require('express').Router();
 const UserController = require('../Controllers/UserController');
-const middleware = require('../middleware');
-const AuthController = require('../Controllers/AuthController');
+const middleware = require('../middleware')
+const AuthController = require('../Controllers/AuthController')
 
-Router.post(
-  '/login',
-  // middleware.stripToken,
-  // middleware.verifyToken,
-  AuthController.Login
-);
-Router.get('/', UserController.getAllUsers);
-// Router.get('/:user_id', UserController.getOneUser);
-// Router.post('/:user_id', UserController.CreateUser);
+ Router.post('/login', AuthController.Login)
+ Router.get('/', UserController.getAllUsers);
+ Router.get('/:user_id', UserController.getOneUser);
+ Router.post('/:user_id', UserController.CreateUser);
 
-Router.post('/register', AuthController.Register);
-Router.put('/login/:user_id/updatepassword',
+Router.post('/register', AuthController.Register)
+Router.get('/session',
     middleware.stripToken,
     middleware.verifyToken,
-    AuthController.UpdatePassword )
-Router.get('/session',
-     middleware.stripToken,
-     middleware.verifyToken,
-     AuthController.CheckSession
+    AuthController.CheckSession
    )
-
+  
 module.exports = Router;
+//comment//
