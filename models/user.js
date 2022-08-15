4eb701b841bf83e816e3440a72d508d929102d88
ViryF <diana13_2011@hotmail.com>
2022-08-15 14:17:09 -0500

    fixed default value on model

--- models/user.js
+++ models/user.js
@@ -18,7 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       username: { type: DataTypes.STRING, allowNull: false },
       email: { type: DataTypes.STRING, allowNull: false, unique: true },
       passwordDigest: { type: DataTypes.STRING, allowNull: false },
-      profilePicture: { type: DataTypes.STRING }
+      profilePicture: {
+        type: DataTypes.STRING,
+        defaultValue:
+          'https://i1.wp.com/wilcity.com/wp-content/uploads/2020/06/115-1150152_default-profile-picture-avatar-png-green.jpg?fit=820%2C860&ssl=1'
+      }
     },
     {
       sequelize,
