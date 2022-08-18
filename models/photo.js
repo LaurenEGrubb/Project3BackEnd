8f7575d638addae70986946c8af4e0198e2b32c3
Austin LePorin <austinleporin@gmail.com>
2022-08-18 14:51:33 -0400

    Finished Backenend

--- models/photo.js
+++ models/photo.js
@@ -2,11 +2,6 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Photo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Photo.belongsTo(models.Album, { foreignKey: 'albumId', as: 'album' })
     }
